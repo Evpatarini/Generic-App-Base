b66fc83b9fd93ea17be83ec22359997a63102510
Evpatarini <epatarini@davinhealthcare.com>
2024-08-20 21:34:09 -0400

    Create CMasterLibrary.php

---
+++ src/CMasterLibrary.php
@@ -0,0 +1,14 @@
+<?php
+
+class CMasterLibrary
+{
+    public static function goodbyeOutput(){
+        return "<h2>My ankle is itchy</h2>";
+    }
+
+    public static function helloOutput(){
+        return "<h2>My ankle is itchy</h2>";
+    }
+}
+
+?>
