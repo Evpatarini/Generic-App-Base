5ed5e27828d3f7164266dc128ffd1ffa00e97c97
Evpatarini <epatarini@davinhealthcare.com>
2024-08-20 21:36:36 -0400

    Update index.php

--- packages/sample/goodbye/index.php
+++ packages/sample/goodbye/index.php
@@ -1,10 +1,10 @@
 <?php
-
+require_once(dirname(__FILE__).'/../../../src/CMasterLibrary.php');
 function main(array $args) : array
 {
   echo "We in here instead";
   return [
-        'body' => 'Something else',
+        'body' => CMasterLibrary::goodbyeOutput(),
     ];
 }
 ?>
