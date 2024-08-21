346626ef13bdd9780f28095769876968973d1f97
Evpatarini <epatarini@davinhealthcare.com>
2024-08-20 21:20:03 -0400

    Update index.php

--- packages/sample/hello/index.php
+++ packages/sample/hello/index.php
@@ -4,7 +4,7 @@ function main(array $args) : array
 {
     $name = $args["name"] ?? "stranger";
     
-    $greeting = "Hello {$name}!";
+    $greeting = "Hello {$name}! Youre a Dummy";
     echo $greeting;
  
     return [
