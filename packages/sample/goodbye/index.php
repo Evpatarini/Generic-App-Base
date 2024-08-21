df54f458992c532bcc27b213a872b392abb43d78
Evpatarini <epatarini@davinhealthcare.com>
2024-08-20 22:15:52 -0400

    Update index.php

--- packages/sample/goodbye/index.php
+++ packages/sample/goodbye/index.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-include('sample-functions-php-helloworld/src/CMasterLibrary.php');
+include('/src/CMasterLibrary.php');
 function main(array $args) : array
 {
   echo CMasterLibrary::goodbyeOutput();
