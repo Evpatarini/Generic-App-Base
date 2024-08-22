56c194aef084f602e0b745127d8e9ef631f99cf5
Evpatarini <epatarini@davinhealthcare.com>
2024-08-22 01:16:31 -0400

    Update index.php

--- packages/sample/goodbye/index.php
+++ packages/sample/goodbye/index.php
@@ -2,8 +2,8 @@
 
 function main(array $args) : array
 {
-    $filePath = __DIR__.'/../../../src/CMasterLibrary.php';
-    $sBody = 'Including file at: ' . $filePath . "\n".__DIR__;
+    $filePath = '/../../../src/CMasterLibrary.php';
+    $sBody = 'Including file at: ' . $filePath . "\n";
 
     if (file_exists($filePath)) {
         require_once $filePath;
